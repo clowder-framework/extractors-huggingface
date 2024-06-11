df4b16fde85269669f1f1c2cacab01c143661ae3
Vismayak Mohanarajan <mohanar2@illinois.edu>
2024-06-11 09:58:48 -0500

    Update segmment_anything.py

--- segment-anything-inference/segmment_anything.py
+++ segment-anything-inference/segmment_anything.py
@@ -2,7 +2,6 @@
 import torch
 import cv2
 from segment_anything import sam_model_registry, SamAutomaticMaskGenerator, SamPredictor
-import matplotlib.pyplot as plt
 import numpy as np
 
 DEVICE = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
@@ -12,37 +11,26 @@ sam = sam_model_registry[MODEL_TYPE](checkpoint="sam_vit_h_4b8939.pth").to(devic
 mask_generator = SamAutomaticMaskGenerator(sam)
 
 # Give the path of your image
-IMAGE_PATH= 'test.jpeg'
+IMAGE_PATH = 'test.jpeg'
 # Read the image from the path
-image= cv2.imread(IMAGE_PATH)
-# Convert to RGB format
+image = cv2.imread(IMAGE_PATH)
+# Convert to RGB format (cv2 uses BGR by default)
 image_rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-
 
 # Generate segmentation mask
 sam_result = mask_generator.generate(image_rgb)
 
-
-# Function that inputs the output and plots image and mask
-def show_output(result_dict,axes=None):
-     if axes:
-        ax = axes
-     else:
-        ax = plt.gca()
-        ax.set_autoscale_on(False)
-     sorted_result = sorted(result_dict, key=(lambda x: x['area']),      reverse=True)
-     # Plot for each segment area
-     for val in sorted_result:
+def show_output(result_dict, original_image):
+    output_image = original_image.copy()  # Make a copy of the original to overlay masks
+    sorted_result = sorted(result_dict, key=(lambda x: x['area']), reverse=True)
+    # Overlay each segment with a random color
+    for val in sorted_result:
         mask = val['segmentation']
-        img = np.ones((mask.shape[0], mask.shape[1], 3))
-        color_mask = np.random.random((1, 3)).tolist()[0]
-        for i in range(3):
-            img[:,:,i] = color_mask[i]
-            ax.imshow(np.dstack((img, mask*0.5)))
-        #Save the image
-        plt.savefig('output.png')
+        color_mask = np.random.randint(0, 256, (1, 3), dtype=np.uint8)  # Random color
+        output_image[mask == 1] = output_image[mask == 1] * 0.5 + color_mask * 0.5
+    return output_image
 
-
-_,axes = plt.subplots(1,2, figsize=(16,16))
-axes[0].imshow(image_rgb)
-show_output(sam_result, axes[1])
+# Create the output image
+output_image = show_output(sam_result, image_rgb)
+output_image_bgr = cv2.cvtColor(output_image.astype('uint8'), cv2.COLOR_RGB2BGR)  # Convert to BGR for saving with OpenCV
+cv2.imwrite('output.png', output_image_bgr)
