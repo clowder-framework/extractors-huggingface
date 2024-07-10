88eb2c3a024b9154c7ef4b75508e75a43fc611f1
Vismayak Mohanarajan <mohanar2@illinois.edu>
2024-07-10 13:17:47 -0500

    Updating file extractor

--- segment-anything-inference/single_image_extractor/segment_anything_file_extractor.py
+++ segment-anything-inference/single_image_extractor/segment_anything_file_extractor.py
@@ -33,13 +33,52 @@ class SegmentAnythingFileExtractor(Extractor):
 
         file_path = resource["local_paths"][0]
         file_id = resource["id"]
+        logger.info("Resource: " + str(resource))
 
         # Load parameters
+        SAVE_IMAGE = True
+        BBOX = None
 
-        #Load file
-        connector.message_process(resource, "Loading contents of file...")
-        with Image.open(file_path) as img:
-            img = np.array(img)
+        if 'parameters' in parameters:
+            params = None
+            logging.info("Received parameters")
+            try:
+                params = json.loads(parameters['parameters'])
+            except TypeError as e:
+                print(f"Failed to load parameters, it's not compatible with json.loads().\nError:{e}")
+                if type(parameters == Dict):
+                    params = parameters['parameters']
+
+            if "SAVE_IMAGE" in parameters:
+                SAVE_IMAGE = parameters["SAVE_IMAGE"]
+            if "bbox" in parameters:
+                BBOX = parameters["bbox"]
+
+        # Check if gpu is available
+        if is_available():
+            logging.warning("GPU is available")
+            actor = SegmentAnything.options(num_gpus=1).remote()
+        else:
+            logging.warning("GPU is not available")
+            actor = SegmentAnything.remote()
+
+        segmented_json_mask = ray.get(actor.generate_mask.remote(file_path, BBOX))
+
+        # Encode the masks as JSON
+        class NumpyEncoder(json.JSONEncoder):
+            def default(self, obj):
+                if isinstance(obj, np.ndarray):
+                    return obj.tolist()
+                return json.JSONEncoder.default(self, obj)
+
+        file_name = resource['name'].split(".")[0]
+        logger.info("File name: " + file_name)
+
+        json_file_name = file_name + "_mask.json"
+        with open(json_file_name, 'w') as f:
+            json.dump(segmented_json_mask, f, cls=NumpyEncoder)
+
+
 
 
 
